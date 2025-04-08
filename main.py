c5ccd411cf2e9718e7a17e5ac1e6691f1c8685f7
andres <af.hidalgo.romero@gmail.com>
2025-04-08 14:31:30 +0200

    first commit studentA

---
+++ main.py
@@ -0,0 +1,13 @@
+import utilitiesA as utilA
+
+def main():
+    a = 7
+    b = 5
+
+    c = utilA.sumaA(a, b)
+
+    print(f"El valor de la suma de a y b es: {c}")
+
+if __name__ == "__main__":
+    main()
+
